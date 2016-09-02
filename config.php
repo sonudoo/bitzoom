62a2436701d30d9a77f734043a68c4d5f1dea853
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2016-09-02 12:25:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $server = "localhost"; //Mostly localhost
 $dbusername = "root"; //Username required to login to database
 $dbpassword = "Bit$%*Zoom2016"; //Password required to login
 $db = "bitzoom"; //Database name
-$connect = mysqli_connect($server, $dbusername, $dbpassword, $db); //Using PHP's mysqli library
+$conn = mysqli_connect($server, $dbusername, $dbpassword, $db); //Using PHP's mysqli library
 
-if (!$connect)
+if (!$conn)
   {
 
   /* If connection to database failed then the user must get an exception. However the user must not know the reason for the error. */
