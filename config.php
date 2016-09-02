7b3d36e52245f51dc2f36db15c042bd0635cc0bd
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2016-09-02 12:19:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 
 /*If the user enters correct data then we connect to SQL database and insert the records */
-$servername = "localhost"; //Mostly localhost
-$username = "root"; //Username required to login to database
-$password = "Bit$%*Zoom2016"; //Password required to login
-$dbname = "bitzoom"; //Database name in which the contact table is available
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$server = "localhost"; //Mostly localhost
+$dbusername = "root"; //Username required to login to database
+$dbpassword = "Bit$%*Zoom2016"; //Password required to login
+$db = "bitzoom"; //Database name
+$connect = mysqli_connect($server, $dbusername, $dbpassword, $db); //Using PHP's mysqli library
 
-if (!$conn)
+if (!$connect)
   {
 
   /* If connection to database failed then the user must get an exception. However the user must not know the reason for the error. */
 
-  die("An unknown error occured.");
+  exit("An unknown error occured.");
   }
 
 ?>
