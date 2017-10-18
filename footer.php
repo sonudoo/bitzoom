9720258389b6c7959af842121d006c7f125fdc76
Sushant <mya.sushant@yahoo.com>
2017-10-18 22:39:09 +0530

    Rebasing for version 2

--- footer.php
+++
@@ -1,3 +0,0 @@
-<!-- This is the footer file which contains data about designers, developers etc. The content of this file appears in the footer of the page -->
-<br><br><br><br><br><br><br><br><br><br><br><br>
-    <div class = ""> </div> <div class = "col-sm-12" style="font-size: 12px;padding-top: 5px; text-align:center;   color:#696969;font-family:Courier New;letter-spacing: 0px"> Developed by students of CSE 2k15, BIT, Ranchi. Best Viewed in Google Chrome(1366 x768) <br> <br> </div> <div class=""></div>
